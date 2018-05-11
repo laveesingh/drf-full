9a30a0cb06c524b2726128d0e51139657c958688
laveesingh <lavees96@gmail.com>
2018-05-11 17:13:47 +0530

    wrote an illustration function/example

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -27,3 +27,45 @@ class SnippetSerializer(serializers.Serializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
+
+
+# Following functions are for illustration purposes only
+
+def working_with_serializers():
+    from snippets.models import Snippet
+    from snippets.serializers import SnippetSerializer
+    from rest_framework.renderers import JSONRenderer
+    from rest_framework.parsers import JSONParser
+
+    code1 = '''foo = "bar"
+    def display(var):
+        print("var:", var)'''
+    code2 = '''const foo = "bar"
+    const display = var => console.log("var:", var)
+    '''
+    # create Snippet instances
+    snippet1 = Snippet(code=code1)
+    snippet2 = Snippet(code=code2)
+
+    # save instances to the db
+    snippet1.save()
+    snippet2.save()
+
+    # create serializer instance
+    serializer = SnippetSerializer(snippet)
+    serializer.data  # => in native types (not exactly but this will do)
+
+    # render the data to json
+    content = JSONRenderer().render(serializer.data)  # => bytes(string)
+
+    # deserialization
+    from django.utils.six import BytesIO
+
+    stream = BytesIO(content)
+    data = JSONParser().parse(stream)  # => absolute native types
+
+    # restore native types to fully populated object instance
+    serializer = SnippetSerializer(data=data)
+    serializer.is_valid()  # => True
+    serializer.validated_data # => almost native types
+    serializer.save() # => save instance to db
