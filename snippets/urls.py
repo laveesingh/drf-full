10c972114efe5f7d7f9201d35a4cc80f3a5da98e
laveesingh <lavees96@gmail.com>
2018-05-12 16:01:47 +0530

    added format suffix support

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
 
@@ -7,5 +8,4 @@ urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>\d+)/$', views.snippet_detail),
 ]
-
-
+urlpatterns = format_suffix_patterns(urlpatterns)
